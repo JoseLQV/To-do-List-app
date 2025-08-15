61d38a573462f94a20e6b51245138bf7aed6befe
Jose Luis Quinones Velez <velezlx1@gmail.com>
2025-08-15 01:40:31 -0400

    Creating dashboard layout , fixing the containers for the dashboard still need progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import RightNavbar from './components/LeftNavbar';
-import TodoList from './components/TodoList';
+import  './dashboard.css'; 
+import LeftNavbar from './components/LeftNavbar';
 import CalendarWidget from './components/CalendarWidget';
 
 function App() {
   return (
-    <div>
-      <CalendarWidget />
-      <TodoList />
-      <RightNavbar />
-    </div>
+      <div className="dashboard-wrapper">
+          <div className="dashboard-left">
+            <LeftNavbar />
+          </div>
+
+          <div className="dashboard-middle">
+            {/* Middle content */}
+          </div>
+
+          <div className="dashboard-right">
+            <CalendarWidget />
+          </div>
+      </div>
   );
 }
 
--- frontend/src/components/LeftNavbar.js
+++ frontend/src/components/LeftNavbar.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
+import '../CSSs_components/LeftNavbar.css'; 
 
-
-function RightNavbar() {
+function LeftNavbar() {
   return (
-    <div style={{
-      height: '100vh',
-      width: '200px',
-      position: 'fixed',
-      left: 0,
-      top: 0,
-      backgroundColor: '#f8f9fa',
-      borderLeft: '1px solid #dee2e6',
-      paddingTop: '20px'
-    }}>
-      <Nav defaultActiveKey="/home" className="flex-column">
-        <Nav.Link href="/home">Home</Nav.Link>
-        <Nav.Link href="/tasks">Tasks</Nav.Link>
-        <Nav.Link href="/calendar">Calendar</Nav.Link>
+    <div className="navbar-container">
+      <Nav className="flex-column">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#profile">Profile</Nav.Link>
+        <Nav.Link href="#settings">Settings</Nav.Link>
       </Nav>
     </div>
   );
 }
 
-export default RightNavbar;
+export default LeftNavbar;
