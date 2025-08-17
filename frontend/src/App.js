05c5d70951e88cc8165dc80a306bd175c31e7120
Jose Luis Quinones Velez <velezlx1@gmail.com>
2025-08-17 00:39:51 -0400

    Redo
    Wrapper and Boxes to fix the position problem inside the front end
    
    Doing things the right way

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,8 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import  './dashboard.css'; 
-import LeftNavbar from './components/LeftNavbar';
-import CalendarWidget from './components/CalendarWidget';
+import React from "react";
+import Dashboard from "./pages/dashboard/dashboard";
 
 function App() {
-  return (
-      <div className="dashboard-wrapper">
-          <div className="dashboard-left">
-            <LeftNavbar />
-          </div>
-
-          <div className="dashboard-middle">
-            {/* Middle content */}
-          </div>
-
-          <div className="dashboard-right">
-            <CalendarWidget />
-          </div>
-      </div>
-  );
+  return <Dashboard />;
 }
 
 export default App;
---
+++ frontend/src/pages/dashboard/dashboard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./dashboard.css";
+
+export default function Dashboard() {
+  return (
+    <body>
+         <div className="page-wrapper">
+            <div className="dashboard-wrapper">
+                <div className="left-side">Left</div>
+                <div className="middle">MIDDLE </div>
+                <div className="right-side">RIGHT</div>
+            </div>
+        
+        </div>
+
+
+
+    </body>
+   
+  )
+
+}
